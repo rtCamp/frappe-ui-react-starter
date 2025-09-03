dd078d89c7a4e65b3965130f3d66b3958b95f90b
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2025-09-03 17:25:46 +0530

    fix: type fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'path';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -18,7 +16,7 @@ export default defineConfig({
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
       storybookTest({
-        configDir: path.join(dirname, '.storybook')
+        configDir: path.join(import.meta.dirname, '.storybook')
       })],
       test: {
         name: 'storybook',
