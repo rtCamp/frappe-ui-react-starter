7ed65beff20e062209f4ac7b760c7e6b1cf770aa
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2025-09-04 12:02:59 +0530

    feat: setup prettier
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook'
 
 import js from '@eslint/js'
 import globals from 'globals'
@@ -8,19 +8,22 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+export default tseslint.config(
+    [
+        globalIgnores(['dist']),
+        {
+            files: ['**/*.{ts,tsx}'],
+            extends: [
+                js.configs.recommended,
+                tseslint.configs.recommended,
+                reactHooks.configs['recommended-latest'],
+                reactRefresh.configs.vite,
+            ],
+            languageOptions: {
+                ecmaVersion: 2020,
+                globals: globals.browser,
+            },
+        },
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-], storybook.configs["flat/recommended"]);
+    storybook.configs['flat/recommended']
+)
