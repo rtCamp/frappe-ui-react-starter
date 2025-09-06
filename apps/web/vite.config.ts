67116682b0f354e44957fd76bd7ee6084b7c5f78
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2025-09-06 17:31:13 +0530

    feat: add prettier config as package
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 import path from 'path'
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -14,33 +13,5 @@ export default defineConfig({
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
-    },
-    test: {
-        projects: [
-            {
-                extends: true,
-                plugins: [
-                    // The plugin will run tests for the stories defined in your Storybook config
-                    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-                    storybookTest({
-                        configDir: path.join(import.meta.dirname, '.storybook'),
-                    }),
-                ],
-                test: {
-                    name: 'storybook',
-                    browser: {
-                        enabled: true,
-                        headless: true,
-                        provider: 'playwright',
-                        instances: [
-                            {
-                                browser: 'chromium',
-                            },
-                        ],
-                    },
-                    setupFiles: ['.storybook/vitest.setup.ts'],
-                },
-            },
-        ],
     },
 })
