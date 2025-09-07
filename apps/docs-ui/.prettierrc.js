0b89629e1eccdf6508a9118ccf7953b97c2062ed
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2025-09-07 20:48:08 +0530

    feat: add storybook app
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

---
+++ apps/docs-ui/.prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+import defaultConfig from '@react-skeleton/config-prettier';
+
+const config = {
+	...defaultConfig,
+};
+
+export default config;
---
+++ apps/docs-ui/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+});
