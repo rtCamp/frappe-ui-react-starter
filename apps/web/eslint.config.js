7c5db8d3b6a7fa677aeb3fb075314f8bbfa1c84f
Imran Sayed <imranhsayed@gmail.com>
2025-09-09 10:26:11 +0530

    Import the base config from eslint package

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,31 +1,16 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook';
 
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import { config as baseConfig } from "@react-skeleton/config-eslint";
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config(
 	[
+		...baseConfig,
 		globalIgnores(['dist']),
 		{
 			files: ['**/*.{ts,tsx}'],
-			extends: [
-				js.configs.recommended,
-				tseslint.configs.recommended,
-				reactHooks.configs['recommended-latest'],
-				reactRefresh.configs.vite,
-			],
-			languageOptions: {
-				ecmaVersion: 2020,
-				globals: globals.browser,
-				parserOptions: {
-					tsconfigRootDir: import.meta.dirname,
-				},
-			},
 		},
 	],
 	storybook.configs['flat/recommended']
