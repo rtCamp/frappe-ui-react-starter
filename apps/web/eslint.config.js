a63f5d76422c22027fa2b4cc24c1b6cdd10eb62f
Imran Sayed <imranhsayed@gmail.com>
2025-09-09 10:26:27 +0530

    Import base config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,7 +1,7 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook';
 
-import { config as baseConfig } from "@react-skeleton/config-eslint";
+import { config as baseConfig } from '@react-skeleton/config-eslint';
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
 
