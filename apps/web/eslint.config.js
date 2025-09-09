885fb30892e5c10dd386fb16e2b042c803db3016
Imran Sayed <imranhsayed@gmail.com>
2025-09-09 10:33:12 +0530

    Add eslint-plugin-react-refresh

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,16 +1,26 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook';
 
+/**
+ * Imports.
+ */
 import { config as baseConfig } from '@react-skeleton/config-eslint';
 import tseslint from 'typescript-eslint';
+import reactRefresh from "eslint-plugin-react-refresh";
 import { globalIgnores } from 'eslint/config';
 
+/**
+ * Export configs.
+ */
 export default tseslint.config(
 	[
 		...baseConfig,
 		globalIgnores(['dist']),
 		{
 			files: ['**/*.{ts,tsx}'],
+			extends: [
+				reactRefresh.configs.vite, // ✅ keep react-refresh for fast refresh
+			],
 		},
 	],
 	storybook.configs['flat/recommended']
