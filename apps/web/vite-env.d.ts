188ad9cf8013842bf110c37947befa6739d92d50
sayedtaqui <sayedwp@gmail.com>
2025-10-30 19:43:20 +0400

    Remove env files

--- apps/web/vite-env.d.ts
+++ apps/web/vite-env.d.ts
@@ -1,16 +1,1 @@
 /// <reference types="vite/client" />
-
-/**
- * Interface ImportMetaEnv.
- */
-interface ImportMetaEnv {
-	readonly VITE_API_URL: string;
-	readonly VITE_FEATURE_FLAG: string;
-}
-
-/**
- * Interface ImportMeta.
- */
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
