d8f9c3253de3e73edc8be8eec3d40bf7e9d8760e
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-11-06 16:19:00 +0530

    Upgrade to frappe-ui-react to  beta2.0

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,4 +1,5 @@
 // Export all components
+//@ts-ignore
 import '@rtcamp/frappe-ui-react/theme';
 export { Tabs, TabsList, TabsTrigger, TabsContent } from './components/tabs';
 export { Card, CardHeader, CardTitle, CardContent } from './components/card';
