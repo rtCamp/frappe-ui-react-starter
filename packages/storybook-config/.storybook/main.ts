2f40e4ba8ca628742d50bd1076d54de296a94d10
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-11-06 20:18:00 +0530

    Refactor design system paths and imports for consistency and improved build process

--- packages/storybook-config/.storybook/main.ts
+++ packages/storybook-config/.storybook/main.ts
@@ -22,9 +22,10 @@ const config: StorybookConfig = {
         return mergeConfig(config, {
             resolve: {
                 alias: {
-                    '@frappe-ui-react-starter/design-system': path.resolve(__dirname, '../../design-system/src'),
+                    '@frappe-ui-react-starter/design-system': path.resolve(__dirname, '../../design-system/dist'),
                 },
             },
+            
         })
     },
 }
